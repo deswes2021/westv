8a8867b173c4127ff1f84391b3af8b1661182b4b
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-05-13 19:10:28 -0600

    Update knales.js

--- script/knales.js
+++ script/knales.js
@@ -1,12 +1,12 @@
 /*--------------------------MENU--------------------------*/
 /*--------------------------MENU--------------------------*/
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/herr.jpg" tipo="Menu" url="herr"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/nacionales.jpg" tipo="Menu" url="nacionales"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/infantiles.jpg" tipo="Menu" url="infantiles"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/variados.jpg" tipo="Menu" url="variados"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/premium.jpg" tipo="Menu" url="premium"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/deportes.jpg" tipo="Menu" url="deportes"
-#EXTINF:-1 nombre="evento01_0" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/247.jpg" tipo="Menu" url="247"
+#EXTINF: nombre="herr" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/herr.jpg" tipo="Menu" url="herr"
+#EXTINF: nombre="nacionales" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/nacionales.jpg" tipo="Menu" url="nacionales"
+#EXTINF: nombre="infantiles" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/infantiles.jpg" tipo="Menu" url="infantiles"
+#EXTINF: nombre="variados" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/variados.jpg" tipo="Menu" url="variados"
+#EXTINF: nombre="premium" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/premium.jpg" tipo="Menu" url="premium"
+#EXTINF: nombre="deportes" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/deportes.jpg" tipo="Menu" url="deportes"
+#EXTINF: nombre="247" logo="https://raw.githubusercontent.com/deswes2021/westv/main/image/247.jpg" tipo="Menu" url="247"
 /*--------------------------MENU--------------------------*/
 /*--------------------------MENU--------------------------*/
 
