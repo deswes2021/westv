795e5b1b272ba4f878e7aee0497945a677df3a89
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-05-13 19:41:52 -0600

    Update knales.js

--- script/knales.js
+++ script/knales.js
@@ -12,6 +12,29 @@
 
 /*--------------------------LIST NACIONALES--------------------------*/
 /*--------------------------LIST NACIONALES--------------------------*/
-  
+#EXTINF: logo="canal1.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal2.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal3.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal4.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal5.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal6.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal7.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal8.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal9.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal10.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal11.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal12.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal19.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal21.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal22.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal23.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal24.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal25.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal26.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal27.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal28.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal29.jpg" tipo="Nacionales" url=""
+#EXTINF: logo="canal30.jpg" tipo="Nacionales" url=""
+
 /*--------------------------LIST TELECLUB--------------------------*/
 /*--------------------------LIST TELECLUB--------------------------*/
